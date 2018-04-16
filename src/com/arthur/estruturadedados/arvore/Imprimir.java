cf952295218a2fe0d5cf131444591bb7e3fb5998
Arthur Pinheiro <arthurpinheirodelima@gmail.com>
2018-04-15 21:10:44 -0300

    Arvore binaria

---
+++ src/com/arthur/estruturadedados/arvore/Imprimir.java
@@ -0,0 +1,15 @@
+package com.arthur.estruturadedados.arvore;
+
+public class Imprimir {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		No.inserir(10);
+		No.inserir(4);
+		No.inserir(8);
+		No.inserir(9);
+		No.inserir(2);
+	}
+
+}
---
+++ src/com/arthur/estruturadedados/arvore/No.java
@@ -0,0 +1,67 @@
+package com.arthur.estruturadedados.arvore;
+
+public class No {
+
+	int valor;
+	No noEsquerda; //apontador para esquerda
+	No noDireita; //apontador para a direita
+	
+	public No (int valor) {
+		this.valor = valor;
+	}
+	
+	public static No raiz; //apontador para a raiz
+	
+	public static void inserir(int valor) {
+		inserir(raiz, valor);
+	}
+	
+	public static void inserir(No node, int valor) {
+		if(node == null) {
+			raiz = new No(valor);
+		
+		} else {
+			if(valor < node.valor) {
+				
+				if(node.noEsquerda != null) {
+					inserir(node.noEsquerda, valor);				
+				}else {
+					System.out.println("Inserindo " + valor + " a esquerda de " + node.valor);
+					node.noEsquerda = new No(valor);
+				}
+				
+			}else {
+				if(node.noDireita != null) {
+					inserir(node.noDireita, valor);
+				}else {
+					System.out.println("Inserindo " + valor + " a direita de " + node.valor);
+					node.noDireita = new No(valor);
+				}
+			}
+		}
+	}
+	
+	public static void preOrdem(No node) {
+		if(node != null) {
+			System.out.println(node.valor + ", ");
+			preOrdem(node.noEsquerda);
+			preOrdem(node.noDireita);
+		}
+	}
+
+	public static void posOrdem(No node) {
+		if(node != null) {
+			posOrdem(node.noEsquerda);
+			posOrdem(node.noDireita);
+			System.out.println(node.valor + ", ");
+		}
+	}
+
+	public static void emOrdem(No node) {
+		if(node != null) {
+			emOrdem(node.noEsquerda);
+			System.out.println(node.valor + ", ");
+			emOrdem(node.noDireita);
+		}
+	}
+}
